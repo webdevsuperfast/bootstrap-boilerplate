02464915f7027f95213d7d0a246aba5ead7dbebb
webdevsuperfast <acob.rotsen@hotmail.com>
2019-11-02 10:07:38 +0800

    Imported needed bootstrap configuration and files

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Bootstrap Configuration
+import '@/config/bootstrap'
+
+// Consilidated SCSS File
+import '@/assets/scss/style.scss'
+
 Vue.config.productionTip = false
 
 new Vue({
